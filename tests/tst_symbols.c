f0ced44192a0bbe32fcab60c1c1656bb7dd7e1e1
Simon Josefsson <simon@josefsson.org>
2022-06-25 19:47:26 +0200

    Revert back to using assert to silence codacity.

--- tests/tst_symbols.c
+++ tests/tst_symbols.c
@@ -33,92 +33,94 @@
 
 #include "utils.h"
 
+static void
+assert_symbol_exists (const void *p)
+{
+  assert (p);
+  if (!p)
+    fail ("symbo NULL?!\n");
+}
+
 void
 doit (void)
 {
-  const void *p;
-
-  p = (const void *) idn_free;
-  p = (const void *) idn_free;
-  p = (const void *) idna_strerror;
-  p = (const void *) idna_to_ascii_4i;
-  p = (const void *) idna_to_ascii_4z;
-  p = (const void *) idna_to_ascii_8z;
-  p = (const void *) idna_to_ascii_lz;
-  p = (const void *) idna_to_unicode_44i;
-  p = (const void *) idna_to_unicode_4z4z;
-  p = (const void *) idna_to_unicode_8z4z;
-  p = (const void *) idna_to_unicode_8z8z;
-  p = (const void *) idna_to_unicode_8zlz;
-  p = (const void *) idna_to_unicode_lzlz;
-  p = (const void *) pr29_4;
-  p = (const void *) pr29_4z;
-  p = (const void *) pr29_8z;
-  p = (const void *) pr29_strerror;
-  p = (const void *) punycode_decode;
-  p = (const void *) punycode_encode;
-  p = (const void *) punycode_strerror;
-  p = (const void *) stringprep;
-  p = (const void *) stringprep_4i;
-  p = (const void *) stringprep_4zi;
-  p = (const void *) stringprep_check_version;
-  p = (const void *) stringprep_convert;
-  p = (const void *) stringprep_iscsi;
-  p = (const void *) stringprep_iscsi_prohibit;
-  p = (const void *) stringprep_kerberos5;
-  p = (const void *) stringprep_locale_charset;
-  p = (const void *) stringprep_locale_to_utf8;
-  p = (const void *) stringprep_nameprep;
-  p = (const void *) stringprep_plain;
-  p = (const void *) stringprep_profile;
-  p = (const void *) stringprep_profiles;
-  p = (const void *) stringprep_rfc3454_A_1;
-  p = (const void *) stringprep_rfc3454_B_1;
-  p = (const void *) stringprep_rfc3454_B_2;
-  p = (const void *) stringprep_rfc3454_B_3;
-  p = (const void *) stringprep_rfc3454_C_1_1;
-  p = (const void *) stringprep_rfc3454_C_1_2;
-  p = (const void *) stringprep_rfc3454_C_2_1;
-  p = (const void *) stringprep_rfc3454_C_2_2;
-  p = (const void *) stringprep_rfc3454_C_3;
-  p = (const void *) stringprep_rfc3454_C_4;
-  p = (const void *) stringprep_rfc3454_C_5;
-  p = (const void *) stringprep_rfc3454_C_6;
-  p = (const void *) stringprep_rfc3454_C_7;
-  p = (const void *) stringprep_rfc3454_C_8;
-  p = (const void *) stringprep_rfc3454_C_9;
-  p = (const void *) stringprep_rfc3454_D_1;
-  p = (const void *) stringprep_rfc3454_D_2;
-  p = (const void *) stringprep_saslprep;
-  p = (const void *) stringprep_saslprep_space_map;
-  p = (const void *) stringprep_strerror;
-  p = (const void *) stringprep_trace;
-  p = (const void *) stringprep_ucs4_nfkc_normalize;
-  p = (const void *) stringprep_ucs4_to_utf8;
-  p = (const void *) stringprep_unichar_to_utf8;
-  p = (const void *) stringprep_utf8_nfkc_normalize;
-  p = (const void *) stringprep_utf8_to_locale;
-  p = (const void *) stringprep_utf8_to_ucs4;
-  p = (const void *) stringprep_utf8_to_unichar;
-  p = (const void *) stringprep_xmpp_nodeprep;
-  p = (const void *) stringprep_xmpp_nodeprep_prohibit;
-  p = (const void *) stringprep_xmpp_resourceprep;
+  assert_symbol_exists ((const void *) idn_free);
+  assert_symbol_exists ((const void *) idna_strerror);
+  assert_symbol_exists ((const void *) idna_to_ascii_4i);
+  assert_symbol_exists ((const void *) idna_to_ascii_4z);
+  assert_symbol_exists ((const void *) idna_to_ascii_8z);
+  assert_symbol_exists ((const void *) idna_to_ascii_lz);
+  assert_symbol_exists ((const void *) idna_to_unicode_44i);
+  assert_symbol_exists ((const void *) idna_to_unicode_4z4z);
+  assert_symbol_exists ((const void *) idna_to_unicode_8z4z);
+  assert_symbol_exists ((const void *) idna_to_unicode_8z8z);
+  assert_symbol_exists ((const void *) idna_to_unicode_8zlz);
+  assert_symbol_exists ((const void *) idna_to_unicode_lzlz);
+  assert_symbol_exists ((const void *) pr29_4);
+  assert_symbol_exists ((const void *) pr29_4z);
+  assert_symbol_exists ((const void *) pr29_8z);
+  assert_symbol_exists ((const void *) pr29_strerror);
+  assert_symbol_exists ((const void *) punycode_decode);
+  assert_symbol_exists ((const void *) punycode_encode);
+  assert_symbol_exists ((const void *) punycode_strerror);
+  assert_symbol_exists ((const void *) stringprep);
+  assert_symbol_exists ((const void *) stringprep_4i);
+  assert_symbol_exists ((const void *) stringprep_4zi);
+  assert_symbol_exists ((const void *) stringprep_check_version);
+  assert_symbol_exists ((const void *) stringprep_convert);
+  assert_symbol_exists ((const void *) stringprep_iscsi);
+  assert_symbol_exists ((const void *) stringprep_iscsi_prohibit);
+  assert_symbol_exists ((const void *) stringprep_kerberos5);
+  assert_symbol_exists ((const void *) stringprep_locale_charset);
+  assert_symbol_exists ((const void *) stringprep_locale_to_utf8);
+  assert_symbol_exists ((const void *) stringprep_nameprep);
+  assert_symbol_exists ((const void *) stringprep_plain);
+  assert_symbol_exists ((const void *) stringprep_profile);
+  assert_symbol_exists ((const void *) stringprep_profiles);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_A_1);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_B_1);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_B_2);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_B_3);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_C_1_1);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_C_1_2);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_C_2_1);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_C_2_2);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_C_3);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_C_4);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_C_5);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_C_6);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_C_7);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_C_8);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_C_9);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_D_1);
+  assert_symbol_exists ((const void *) stringprep_rfc3454_D_2);
+  assert_symbol_exists ((const void *) stringprep_saslprep);
+  assert_symbol_exists ((const void *) stringprep_saslprep_space_map);
+  assert_symbol_exists ((const void *) stringprep_strerror);
+  assert_symbol_exists ((const void *) stringprep_trace);
+  assert_symbol_exists ((const void *) stringprep_ucs4_nfkc_normalize);
+  assert_symbol_exists ((const void *) stringprep_ucs4_to_utf8);
+  assert_symbol_exists ((const void *) stringprep_unichar_to_utf8);
+  assert_symbol_exists ((const void *) stringprep_utf8_nfkc_normalize);
+  assert_symbol_exists ((const void *) stringprep_utf8_to_locale);
+  assert_symbol_exists ((const void *) stringprep_utf8_to_ucs4);
+  assert_symbol_exists ((const void *) stringprep_utf8_to_unichar);
+  assert_symbol_exists ((const void *) stringprep_xmpp_nodeprep);
+  assert_symbol_exists ((const void *) stringprep_xmpp_nodeprep_prohibit);
+  assert_symbol_exists ((const void *) stringprep_xmpp_resourceprep);
 
 #ifdef WITH_TLD
-  p = (const void *) tld_check_4;
-  p = (const void *) tld_check_4t;
-  p = (const void *) tld_check_4tz;
-  p = (const void *) tld_check_4z;
-  p = (const void *) tld_check_8z;
-  p = (const void *) tld_check_lz;
-  p = (const void *) tld_default_table;
-  p = (const void *) tld_get_4;
-  p = (const void *) tld_get_4z;
-  p = (const void *) tld_get_table;
-  p = (const void *) tld_get_z;
-  p = (const void *) tld_strerror;
+  assert_symbol_exists ((const void *) tld_check_4);
+  assert_symbol_exists ((const void *) tld_check_4t);
+  assert_symbol_exists ((const void *) tld_check_4tz);
+  assert_symbol_exists ((const void *) tld_check_4z);
+  assert_symbol_exists ((const void *) tld_check_8z);
+  assert_symbol_exists ((const void *) tld_check_lz);
+  assert_symbol_exists ((const void *) tld_default_table);
+  assert_symbol_exists ((const void *) tld_get_4);
+  assert_symbol_exists ((const void *) tld_get_4z);
+  assert_symbol_exists ((const void *) tld_get_table);
+  assert_symbol_exists ((const void *) tld_get_z);
+  assert_symbol_exists ((const void *) tld_strerror);
 #endif
-
-  if (p == NULL)
-    fail ("no symbols");
 }
